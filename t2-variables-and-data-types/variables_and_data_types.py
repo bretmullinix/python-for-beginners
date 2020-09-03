f23c1083f3d76cc43d1701f57f9158f8bf575afa
Bret Mullinix <bmullini@redhat.com>
2020-09-03 08:23:43 -0400

    added finished class from tutorial

---
+++ t2-variables-and-data-types/variables_and_data_types.py
@@ -0,0 +1,26 @@
+# Working with integers--->
+current_age = 35
+print('I am', current_age, 'years old.')
+add_years = 10
+future_age = current_age + add_years
+print('In', add_years, 'years,', 'I will be', future_age )
+
+# Separate your data type output with an empty line
+print('')
+# Working with floats --->
+first_angle_in_triangle = 28.9
+second_angle_in_triangle = 45.5
+third_angle_in_triangle = 180 - first_angle_in_triangle - second_angle_in_triangle
+
+print('A triangle has 3 angles that add up to 180 degrees.')
+print('The first angle for our triangle is', first_angle_in_triangle, 'degrees.')
+print ('The second angle for our triangle is', second_angle_in_triangle, 'degrees.')
+print('The third angle is', third_angle_in_triangle, 'degrees.')
+print('Our third angle was calculated based on the knowledge that a triangle adds up to 180 degrees.')
+
+print('')
+# Working with Strings
+first_name_of_a_person = "Mike"
+last_name_of_a_person = "Smith"
+full_name_of_a_person = first_name_of_a_person + " " + last_name_of_a_person
+print('Full name =', full_name_of_a_person)
