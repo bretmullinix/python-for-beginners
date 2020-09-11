d4e03a8b717691d69949748dfc7708a8d8c75a5d
Bret Mullinix <bmullini@redhat.com>
2020-09-11 15:44:50 -0400

    updated the resulting artifacts

--- t6-tdd/database.py
+++ t6-tdd/database.py
@@ -1,6 +1,9 @@
+import time
+
 class Database:
     def __init__(self, p_database_type):
         self.database_type = p_database_type
 
     def get_all_employees(self):
+        # time.sleep( 60 ) # Uncomment this code for the exercise in similating a long database query....
         return ["employee1", "employee2"]
--- t6-tdd/tests/list_all_employees.py
+++ t6-tdd/tests/list_all_employees.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 from database import Database
 
@@ -6,6 +7,15 @@ from database import Database
 class TestListAllEmployees(unittest.TestCase):
 
     def test_list_all_employees(self):
+        database = Database("postgres")
+        employees = database.get_all_employees()
+        print("Your list of employees --->")
+        for employee in employees:
+            print(employee)
+
+    @patch('database.Database.get_all_employees')
+    def test_list_all_employees_fast(self, mock_method_get_all_employees ):
+        mock_method_get_all_employees.return_value =  ['employee4', 'employee5']
         database = Database("postgres")
         employees = database.get_all_employees()
         print("Your list of employees --->")
